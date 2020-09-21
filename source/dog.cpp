a30a08ad5fb1034d12ed06b8977f3b6759cb26a3
Jamie Orchard <42879972+JamieOrchard@users.noreply.github.com>
2020-09-21 23:44:39 +0100

    Updated Dog with eventhandling

--- source/dog.cpp
+++ source/dog.cpp
@@ -6,6 +6,18 @@ void DogCreate()
 void DogUpdate()
 {
 
+	if(IsKeyDown(SDL_SCANCODE_D) || IsKeyDown(SDL_SCANCODE_RIGHT)){gDog.right = true;}
+	if(IsKeyDown(SDL_SCANCODE_A) || IsKeyDown(SDL_SCANCODE_LEFT)){gDog.left = true;}
+	if(IsKeyPressed(SDL_SCANCODE_W) || IsKeyPressed(SDL_SCANCODE_UP) || IsKeyPressed(SDL_SCANCODE_SPACE) || IsKeyPressed(SDL_SCANCODE_Z)){gDog.jump = true;}
+	if(IsKeyPressed(SDL_SCANCODE_S) || IsKeyPressed(SDL_SCANCODE_DOWN) || IsKeyPressed(SDL_SCANCODE_E) || IsKeyPressed(SDL_SCANCODE_X)){gDog.action = true;}
+
+	if(gDog.right){gDog.vel_x += 2;}
+
+
+
+
+
+
 }
 
 void DogDraw()
--- source/dog.hpp
+++ source/dog.hpp
@@ -8,6 +8,14 @@ GLOBAL struct Dog
 	float vel_x;
 	float vel_y;
 
+	bool left = false;
+	bool right = false;
+	bool jump = false;
+	bool action = false;
+
+	const float max_vel_x = 10;
+	const float max_vel_y = 10;
+
 	const int width = 24;
 	const int height = 24;
 
