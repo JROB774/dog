d1659f842ca8b1fa9e61a697ed40ba6326c246dc
Jamie Orchard <42879972+JamieOrchard@users.noreply.github.com>
2020-09-22 12:11:24 +0100

    Updated collision code

--- source/dog.cpp
+++ source/dog.cpp
@@ -29,20 +29,20 @@ INTERNAL void CreateDog (Dog& dog, float x, float y)
 
 INTERNAL void UpdateDog (Dog& dog, float dt)
 {
-	dog.right = IsKeyDown(SDL_SCANCODE_RIGHT);
-	dog.left  = IsKeyDown(SDL_SCANCODE_LEFT);
+	dog.right = (IsKeyDown(SDL_SCANCODE_RIGHT) || IsKeyDown(SDL_SCANCODE_D));
+	dog.left  = (IsKeyDown(SDL_SCANCODE_LEFT)  || IsKeyDown(SDL_SCANCODE_A));
 
 	// Only if one direction is being pressed will we move the dog.
 	if (dog.right != dog.left)
 	{
-		if (dog.right) { dog.pos.x += (DOG_MOVE_SPEED * dt); dog.flip = FLIP_NONE; } // Move right.
-		if (dog.left ) { dog.pos.x -= (DOG_MOVE_SPEED * dt); dog.flip = FLIP_HORZ; } // Move left.
+		if (dog.right) { dog.vel.x += (DOG_MOVE_SPEED * dt); dog.flip = FLIP_NONE; } // Move right.
+		if (dog.left ) { dog.vel.x -= (DOG_MOVE_SPEED * dt); dog.flip = FLIP_HORZ; } // Move left.
 	}
 
 	// Apply a jump force if the key is presed.
 	if (dog.grounded)
 	{
-		if (IsKeyPressed(SDL_SCANCODE_Z))
+		if (IsKeyPressed(SDL_SCANCODE_Z) || IsKeyPressed(SDL_SCANCODE_SPACE))
 		{
 			dog.vel.y -= DOG_JUMP_FORCE;
 			dog.grounded = false;
@@ -50,17 +50,17 @@ INTERNAL void UpdateDog (Dog& dog, float dt)
 	}
 
 	// Apply a gravity force to the dog.
-	if (!dog.grounded) dog.vel.y += GRAVITY; else dog.vel.y = 0.0f;
+	//if (!dog.grounded) dog.vel.y += GRAVITY; else dog.vel.y = 0.0f;
+	dog.vel.y += GRAVITY;
 
 	// Clamp the velocity in range.
-	// if (dog.vel.y < -DOG_MAX_VEL) dog.vel.y = -DOG_MAX_VEL;
-	// if (dog.vel.y >  DOG_MAX_VEL) dog.vel.y =  DOG_MAX_VEL;
-	// if (dog.vel.x < -DOG_MAX_VEL) dog.vel.x = -DOG_MAX_VEL;
-	// if (dog.vel.x >  DOG_MAX_VEL) dog.vel.x =  DOG_MAX_VEL;
+	if (dog.vel.y < -DOG_MAX_VEL) dog.vel.y = -DOG_MAX_VEL;
+	if (dog.vel.y >  DOG_MAX_VEL) dog.vel.y =  DOG_MAX_VEL;
+	if (dog.vel.x < -DOG_MAX_VEL) dog.vel.x = -DOG_MAX_VEL;
+	if (dog.vel.x >  DOG_MAX_VEL) dog.vel.x =  DOG_MAX_VEL;
 
-	// Apply velocity to the dog.
-	dog.pos.x += (dog.vel.x * dt);
-	dog.pos.y += (dog.vel.y * dt);
+	Vec2 temp_v = {dog.pos.x + (dog.vel.x * dt), dog.pos.y + (dog.vel.y * dt)};
+	printf("Dog Y Velocity 1: %f\n", dog.vel.y);
 
 	// Perform simple collision detection on the dog.
 	for (int iy=0; iy<gGameState.map.h; ++iy)
@@ -70,22 +70,36 @@ INTERNAL void UpdateDog (Dog& dog, float dt)
 			Tile* tile = &gGameState.map.tiles[iy*gGameState.map.w+ix];
 			if (tile->type == TILE_SOLID)
 			{
+				Vec2 temp_2 = {temp_v.x, dog.pos.y};
 				Rect intersection;
-				if (TileEntityCollision(dog.pos,dog.bounds, ix,iy, intersection))
+				if (TileEntityCollision(temp_2,dog.bounds, ix,iy, intersection))
 				{
-					if (intersection.w < intersection.h)
-					{
-						dog.pos.x -= intersection.w;
-					}
-					else
-					{
-						dog.pos.y -= intersection.h;
-						dog.grounded = true;
-					}
+					if(dog.vel.x > 0){dog.pos.x -= intersection.w;}
+					if(dog.vel.x < 0){dog.pos.x += intersection.w;}
+					dog.vel.x = 0;
+				}
+				temp_2 = {dog.pos.x, temp_v.y};
+				if (TileEntityCollision(temp_2,dog.bounds, ix,iy, intersection))
+				{
+					printf("==========Report===========\n");
+					printf("INTERSECTION SIZE: %f\n", intersection.h);
+					printf("Character Bounds: %f, %f, %f, %f\n", dog.pos.x + dog.bounds.x , temp_v.y + dog.bounds.y, dog.bounds.w, dog.bounds.h);
+					printf("Tile Bounds:      %d, %d, %d, %d\n", ix * 16, iy * 16, 16, 16);
+					
+					if(dog.vel.y > 0){dog.pos.y -= intersection.h;}
+					if(dog.vel.y < 0){dog.pos.y += intersection.h;}
+					dog.vel.y = 0;
+					dog.grounded = true;
+
+					printf("New Character Bounds: %f, %f, %f, %f\n", dog.pos.x + dog.bounds.x , dog.pos.y + dog.bounds.y, dog.bounds.w, dog.bounds.h);
 				}
 			}
 		}
 	}
+
+	// Apply velocity to the dog.
+	dog.pos.x += (dog.vel.x * dt);
+	dog.pos.y += (dog.vel.y * dt);
 }
 
 INTERNAL void DrawDog (Dog& dog, float dt)
