631d50d7cb501eb7c26a0fe3a714e00c27d28308
Jamie Orchard <42879972+JamieOrchard@users.noreply.github.com>
2020-09-25 00:10:21 +0100

    Added bone creation

--- source/bone.cpp
+++ source/bone.cpp
@@ -6,20 +6,46 @@ void InitBones()
 
 bool SmallBoneCollision(Rect _bounds, SmallBone _bone)
 {
-	if((_bounds.x + _bounds.w > _bone.bounds.x) && (_bounds.y +_bounds.h > _bone.bounds.y) &&
-		(_bounds.x < _bone.bounds.x + _bone.bounds.w) && (_bounds.y < _bone.bounds.y + _bone.bounds.h)){
-			return true;
+	if(!_bone.dead){
+		if((_bounds.x + _bounds.w > _bone.bounds.x) && (_bounds.y +_bounds.h > _bone.bounds.y) &&
+			(_bounds.x < _bone.bounds.x + _bone.bounds.w) && (_bounds.y < _bone.bounds.y + _bone.bounds.h)){
+				return true;
+		}
 	}
 	return false;
 }
 
 bool BigBoneCollision(Rect _bounds, BigBone _bone)
 {
-	if((_bounds.x + _bounds.w > _bone.bounds.x) && (_bounds.y +_bounds.h > _bone.bounds.y) &&
-		(_bounds.x < _bone.bounds.x + _bone.bounds.w) && (_bounds.y < _bone.bounds.y + _bone.bounds.h)){
-			return true;
+	if(!_bone.dead){
+		if((_bounds.x + _bounds.w > _bone.bounds.x) && (_bounds.y +_bounds.h > _bone.bounds.y) &&
+			(_bounds.x < _bone.bounds.x + _bone.bounds.w) && (_bounds.y < _bone.bounds.y + _bone.bounds.h)){
+				return true;
+		}
 	}
 	return false;
+}
+
+void CreateSmallBone(SmallBone& _bone, float _x, float _y)
+{
+	_bone.bounds = {_x, _y, 10, 10};
+	_bone.dead = false;
+}
+
+void CreateBigBone(BigBone& _bone, float _x, float _y)
+{
+	_bone.bounds = {_x, _y, 16, 16};
+	_bone.dead = false;
+}
+
+void RenderBigBone(BigBone& _bone)
+{
+	DrawImage(big_bone_image, _bone.bounds.x, _bone.bounds.y);
+}
+
+void RenderSmallBone(SmallBone& _bone)
+{
+	DrawImage(small_bone_image, _bone.bounds.x, _bone.bounds.y);
 }
 
 void RenderBoneCounter(int _x, int _y, BoneCounter _bonecounter)
--- source/bone.hpp
+++ source/bone.hpp
@@ -10,20 +10,24 @@ struct BoneCounter
 struct SmallBone
 {
 	Rect bounds = {0,0,10,10};
+	bool dead = false;
 };
 
 struct BigBone
 {
 	Rect bounds = {0,0,16,16};
+	bool dead = false;
 };
 
 GLOBAL Image small_bone_image;
 GLOBAL Image big_bone_image;
 
 void InitBones();
+void CreateSmallBone(SmallBone& _bone, float _x, float _y);
+void CreateBigBone(BigBone& _bone, float _x, float _y);
 bool SmallBoneCollision(Rect _bounds, SmallBone _bone);
 bool BigBoneCollision(Rect _bounds, BigBone _bone);
-void RenderBoneCounter(int _x, int _y);
+void RenderBoneCounter(int _x, int _y, BoneCounter _bonecounter);
 void DeleteBones();
 
 #endif
