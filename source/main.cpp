2912fd71bdef9c774ec1c1b939dac29ba8cdae2b
Joshua Robertson <jrobertson774@yahoo.com>
2020-09-25 12:24:04 +0100

    Pressing F5 will now restart/reset the game in debug builds.

--- source/main.cpp
+++ source/main.cpp
@@ -12,44 +12,65 @@ INTERNAL void QuitApplication ()
 int main (int argc, char** argv)
 {
     ErrorTerminateCallback = QuitApplication;
-    ErrorMaximumCallback = QuitApplication;
 
     gWindow.running = true;
 
-    RandomSeed();
-
     InitErrorSystem();
-
     InitWindow();
     InitMixer();
-    InitGame();
-
     InitFrameTimer();
 
-    ShowWindow();
-
-    while (gWindow.running)
+    if (gWindow.running)
     {
-        UpdateKeyboardState();
-
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+        bool reset = true;
+        while (reset)
         {
-            if (event.type == SDL_QUIT)
+            gWindow.running = true;
+            reset = false;
+
+            RandomSeed();
+            InitGame();
+            ShowWindow();
+
+            while (gWindow.running)
             {
-                gWindow.running = false;
+                UpdateKeyboardState();
+
+                SDL_Event event;
+                while (SDL_PollEvent(&event))
+                {
+                    switch (event.type)
+                    {
+                        #if defined(BUILD_DEBUG)
+                        case (SDL_KEYDOWN):
+                        {
+                            if (event.key.keysym.sym == SDLK_F5)
+                            {
+                                gWindow.running = false;
+                                reset = true;
+                            }
+                        } break;
+                        #endif
+                        case (SDL_QUIT):
+                        {
+                            gWindow.running = false;
+                        } break;
+                    }
+                }
+
+                ClearWindow(MakeColor(0,0,0));
+                SetViewport();
+
+                UpdateGame(gTimer.delta_time);
+                RenderGame(gTimer.delta_time);
+
+                CapFramerate();
+
+                RefreshWindow();
             }
+
+            QuitGame();
         }
-
-        ClearWindow(MakeColor(0,0,0));
-        SetViewport();
-
-        UpdateGame(gTimer.delta_time);
-        RenderGame(gTimer.delta_time);
-
-        CapFramerate();
-
-        RefreshWindow();
     }
 
     QuitApplication();
