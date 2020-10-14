eb0b73e1f43aea61b29e1191077d881da7f95d10
Joshua Robertson <jrobertson774@yahoo.com>
2020-10-14 03:47:22 +0100

    Minor changes.

--- source/application.cpp
+++ source/application.cpp
@@ -31,8 +31,8 @@ INTERNAL void QuitApplication ()
 
 INTERNAL void UpdateApplication (float dt)
 {
-    if (IsKeyPressed(SDL_SCANCODE_F) || IsKeyPressed(SDL_SCANCODE_F11)){SetFullscreen(!IsFullscreen());}
-    
+    if (IsKeyPressed(SDL_SCANCODE_F) || IsKeyPressed(SDL_SCANCODE_F11)) SetFullscreen(!IsFullscreen());
+
     if (!IsFading())
     {
         switch (gAppState.state)
