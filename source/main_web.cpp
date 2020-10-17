070d9bf122536c1cd3188d78b98ac2ecfd154479
Joshua Robertson <jrobertson774@yahoo.com>
2020-10-17 11:50:50 +0100

    Added main header and source files for the web build.

---
+++ source/main_web.cpp
@@ -0,0 +1,6 @@
+#include "main_web.hpp"
+
+int main (int argc, char** argv)
+{
+    return 0;
+}
---
+++ source/main_web.hpp
@@ -0,0 +1,6 @@
+#ifndef MAIN_WEB_HPP
+#define MAIN_WEB_HPP
+
+// Nothing...
+
+#endif /* MAIN_WEB_HPP */
